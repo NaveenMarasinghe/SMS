b567960203fb5f1e520d4dd097537892f0733530
NaveenMare <naveenmareftw@gmail.com>
2022-12-10 18:57:28 +0530

    Added StudentsController

---
+++ SMS/Controllers/StudentsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SMS.Services;
+using System.Linq;
+
+namespace SMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IStudentRepository _studentService;
+
+        public StudentsController(IStudentRepository repository)
+        {
+            _studentService = repository;
+        }
+
+        [HttpGet("{id?}")]
+        public IActionResult GetStudents(int? id) 
+        {
+            var studentsData = _studentService.AllStudents();
+
+            if (id is null) return Ok(studentsData);
+
+            studentsData = studentsData.Where(t =>t.Id == id).ToList();
+
+            return Ok(studentsData);
+        }
+    }
+}
