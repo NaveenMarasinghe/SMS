63fa91da7647e7c598d0d22eb6a599d133675ce9
NaveenMare <naveenmareftw@gmail.com>
2022-12-10 18:30:26 +0530

    Added Student, Grade and Subject data models

---
+++ SMS.Models/Grade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Models
+{
+    public class Grade
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Min_mark { get; set; }
+        public int Max_mark { get; set;}
+    }
+}
---
+++ SMS.Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string NameWithInitials { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }      
+    }
+}
